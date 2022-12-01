30c59a0836f890e16d284a375df83d57c6a7c8c2
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-12-01 17:46:07 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Login from "./Login";
+var isLoggedIn = false;
+
+function App() {
+  return <div className="container">{ 
+   isLoggedIn === true? <h1>Hello</h1>:<Login />
+  }</div>;
+}
+export default App;
